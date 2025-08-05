bb1ae4b90cda65f0f8d01ec429c54d63dcdb0636
dylanmsk <kms920612@gmail.com>
2025-08-05 17:01:44 +0900

    change base layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,33 @@
+import { StyledStack } from "@/components/navigation/stack";
 import "@/global.css";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
 import { Stack } from "expo-router";
+import { useColorScheme } from "react-native";
 
-export default function RootLayout() {
-  return <Stack />;
-}
+const InitialLayout = () => {
+  return (
+    <StyledStack
+      contentClassName="bg-gray-100 dark:bg-dark"
+      headerClassName="bg-dark text-white"
+    >
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+    </StyledStack>
+  );
+};
+
+// Bunch of providers
+const RootLayout = () => {
+  const colorScheme = useColorScheme();
+
+  return (
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      <InitialLayout />
+    </ThemeProvider>
+  );
+};
+
+export default RootLayout;
