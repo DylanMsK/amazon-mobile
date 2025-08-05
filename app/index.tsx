8b8226a4171503e0e8138ee262e9b585b068bbb5
dylanmsk <kms920612@gmail.com>
2025-08-05 11:12:27 +0900

    prebuild

--- app/index.tsx
+++ app/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text className="text-white">
+      <Text className="text-red-500">
         Edit app/index.tsx to edit this screen.
       </Text>
     </View>
